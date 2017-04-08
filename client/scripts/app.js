9e34da2e2b21bb449a0fac7a4489dbe12eb6b697
Hack Reactor Student <student@hackreactor.com>
2017-04-08 12:32:51 -0700

    added filter button for rooms and regex escape to prevent hacks

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,8 +6,50 @@ var App = function() {
 }
 
 App.prototype.init = function() {
-    this.fetch()
+    var that = this;
+    this.fetch(this.renderMessage);
 
+    $('body').on('click', '.chat-submit', function(){
+        var message = {
+            text: $('.chat-input').val(),
+            room: 'room',
+            username: ''
+        }
+        that.send(message);
+    });
+
+    
+    // get value of clicked item
+    $('.rooms').change(function(){
+        $('.hide').removeClass('hide');
+        var value = $(this).val().split(" ")[0];
+        if (value === 'allrooms') {
+            $('.hide').removeClass('hide');
+        } else {
+            $(`.${value}`).addClass("hide");
+        }
+
+    });
+
+    // add class of .hide to all children of rooms
+    // remove class on clicked item
+
+
+    $('.rooms').on('click', '.chat-submit', function(){
+        var message = {
+            text: $('.chat-input').val(),
+            room: 'room',
+            username: ''
+        }
+        that.send(message);
+    });
+
+    this.fetch(this.saveRooms);
+
+    // $('.rooms').append(`<option>${room}</option>`)    
+
+    this.fetch(this.renderMessage);
+    //this.fetch(this.saveRooms);
 };
 
 App.prototype.send = function(message) {
@@ -27,10 +69,9 @@ App.prototype.send = function(message) {
     });
 };
 
-var output = 'asdf';
-var promise = null;
 
-App.prototype.fetch = function(cb) {
+
+App.prototype.fetch = function(callback) {
 
     $.ajax({
         // This is the url you should use to communicate with the parse API server.
@@ -40,9 +81,9 @@ App.prototype.fetch = function(cb) {
         data: {},
         contentType: 'application/json',
         success: function(data) {
-            cb(data);
-            console.log(data);
-            promise = data;
+            dataX.push(data);
+            callback(data);
+            //console.log(data);
         },
 
         error: function(data) {
@@ -55,31 +96,79 @@ App.prototype.fetch = function(cb) {
 };
 
 
-//console.log(output);
+App.prototype.filter = function(message){
+    if (message === undefined) {
+        return '';
+    }
+    return message.replace(/([<>()[{*+.$^\\|?])/g, '\\$1');
+}
 
+//console.log(output);
 
 
 App.prototype.clearMessages = function() {
     $('#chats').empty();
 };
 
+
 App.prototype.renderMessage = function(message) {
 // $('#chats').append("<div>" + message + "</div>");
+console.log(message)
     for (var i = 0; i < message.results.length; i++) {
-        $('#chats').append("<div><p>" + message.results[i].username +
-            "</p><p>" + message.results[i].text + "</p><p>" + message.results[i].createdAt + "</p></div>");
+        var chat = message.results[i];
+        var time = moment(chat.createdAt);
+        var filteredText = app.filter(chat.text);
+        $('#chats').append(`
+            <div class="${chat.roomname}">
+                <p>${chat.username}</p>
+                <p>${filteredText}</p>
+                <p>${time.fromNow()}</p>
+            </div>`);
     }
 };
 
 
-var app = new App();
-var cb = function(message){
-	console.log(message);
-	//app.renderMessage(message);
+App.prototype.saveRooms = function(data) {
+  var rooms = {};
+  _.each(data.results, function(item) {  
+    if (!Object.prototype.hasOwnProperty.call(rooms, item.roomname)) {
+       rooms[item.roomname] = item.roomname; 
+    } 
+  });
+  
+  for (var key in rooms) {
+    $('.rooms').append(`<option class="${key}">${rooms[key]}</option>`); 
+  }
+
+   
 }
 
-app.fetch(app.renderMessage);
-console.log(app.fetchedData)
+
+var app = new App();
+app.init();
+
+
+console.log(dataX);
+// var cb = function(message){
+//     console.log(message);
+//     //app.renderMessage(message);
+// }
+
+
+
+// var result = app.saveRooms([{
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// }, {
+//   username: 'sharkesforcheap',
+//   text: 'trolls',
+//   roomname: '4chan'
+// }])
+
+// console.log(result)
+// app.fetch(app.renderMessage);
+// console.log(app.fetchedData)
 
 
 
